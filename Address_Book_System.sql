c01c4072976e8854b31ba932813ae32caf8edb00
shalini <shalini.v@tvsnext.io>
2021-07-31 22:04:01 +0530

    [shalini] Retrieve value based on city or state

--- Address_Book_System.sql
+++ Address_Book_System.sql
@@ -32,3 +32,8 @@ where FirstName='Rujula' and LastName='Mathialagan';
 delete 
 from Address_Book_Table
 where FirstName='Anita' and SecondName='Yadav'
+------ UC 6: Ability to Retrieve Person belonging to a City or State ------
+
+select * 
+from Address_Book_Table
+where City='Bangalore' or State='Karnataka'
