1fbc8ca3aad0eb2951b8aedb30aa1098d7ea541f
shalini <shalini.v@tvsnext.io>
2021-08-01 23:13:37 +0530

    [shalini]add addr name and relationship type

--- Address_Book_System.sql
+++ Address_Book_System.sql
@@ -4,3 +4,23 @@ select *
 from Address_Book_Table
 where City='Bangalore'
 order by(FirstName)
+------ UC 9: Identify each Address Book with name andType ------
+
+alter table Address_Book_Table
+add AddressBookName varchar(100),
+Type varchar(100)
+
+--Update values for Type=Friends--
+update Address_Book_Table
+set AddressBookName='FriendName',Type='Friends'
+where FirstName='Harsha' or FirstName='Rujula'
+
+--Update values for Type=Family--
+update Address_Book_Table
+set AddressBookName='Mom',Type='Family'
+where FirstName='Pramela'
+
+--Update values for Type=Profession--
+update Address_Book_Table
+set AddressBookName='Manager',Type='Profession'
+where FirstName='Meena'
